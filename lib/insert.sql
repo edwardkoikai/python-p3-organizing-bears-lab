2e08af9ff6906185e3df302dd0113e5fb01a00a8
Edward Koikai <edwardkoikai@gmail.com>
2024-06-05 18:52:21 +0300

    Add query selectors

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 18, "M", "Red", "Sociable",  0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 20, "M", "Green", "Difficult",  1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 22, "M", "Blue", "Easy",  1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 24, "M", "Yellow", "Easy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 26, "M", "Dark", "Easy",  1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 28, "M", "Red", "Difficult",  0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 30, "M", "White", "Easy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 32, "M", "Dark", "Easy", 1);
